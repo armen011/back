8bd2b6bc197cd0f8852cd317549551be473ffc98
Armen <armen21mkrtchyan0616@gmail.com>
2022-11-23 02:27:45 +0400

    add test route

--- routes/auth.js
+++ routes/auth.js
@@ -3,24 +3,20 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //REGISTER
-router.post("/register", async (reqs, res) => {
-	const req={
-		body:{
-			username:"armen21",
-			password:"Armen.21.06",
-			email:'armen21mkrtchyan0616@gmail.com'
-		}
-	}
-	res.status(200).json({bosy:{name:"Armen",body:reqs.body}});
-
+router.post("/register", async (req, res) => {
+	// const req={
+		// body:{
+		// 	username:"armen21",
+		// 	password:"Armen.21.06",
+		// 	email:'armen21mkrtchyan0616@gmail.com'
+		// }
+	// }
 
 	try {
 		//generate new password
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(req.body.password, salt);
 		const emailLowerCase = await req.body.email.toLowerCase();
-
-		console.log("AAAA",emailLowerCase)
 
 		//create new user
 		const newUser = new User({
@@ -33,53 +29,27 @@ router.post("/register", async (reqs, res) => {
 		const user = await newUser.save();
 		res.status(200).json(user);
 	} catch (err) {
-		console.log("err",err)
+		console.log("body",req.body)
+		console.log("query",req.query)
 
 		res.status(500).json(err);
 	}
 });
 
-// //LOGIN
-// router.post("/login", async (reqp, res) => {
+//LOGIN
+router.post("/login", async (reqs, res) => {
 	const req={
 		body:{
 			password:"Armen.21.06",
 			email:'armen21mkrtchyan0616@gmail.com'
 		}
 	}
-// 	try {
-// 		const user = await User.findOne({
-// 			email: req.body.email.toLowerCase(),
-// 		});
-
-// 		!user && res.status(404).json("user not found");
-
-// 		const validPassword = await bcrypt.compare(
-// 			req.body.password,
-// 			user.password
-// 		);
-// 		!validPassword && res.status(400).json("wrong password");
-
-// 		res.status(200).json(user);
-// 	} catch (err) {
-// 		res.status(500).json(err);
-// 	}
-// });
-
-
-// test
-router.post("/login", async (req, res) => {
-	const req={
-		body:{
-			password:"Armen.21.06",
-			email:'armen21mkrtchyan0616@gmail.com'
-		}
-	}
-			const user = await User.findOne({
+	try {
+		const user = await User.findOne({
 			email: req.body.email.toLowerCase(),
 		});
 
-				!user && res.status(404).json("user not found");
+		!user && res.status(404).json("user not found");
 
 		const validPassword = await bcrypt.compare(
 			req.body.password,
@@ -87,7 +57,10 @@ router.post("/login", async (req, res) => {
 		);
 		!validPassword && res.status(400).json("wrong password");
 
-		res.status(200).json({armen:"Armen"});
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
