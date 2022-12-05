076a78084ffdc47d68ec5c80cb2fda03c788aa17
Artur <artursimonyan601@gmail.com>
2022-12-06 01:47:35 +0400

    backend update

--- socket.js
+++ socket.js
@@ -1,30 +1,28 @@
-const broadCast = require("./actions/broadCast")
-const { sendMessage } = require("./actions/messageActions")
+const broadCast = require("./actions/broadCast");
+const { sendMessage } = require("./actions/messageActions");
 
-const activeUsers=new Map()
+const activeUsers = new Map();
 
 module.exports = (io) => {
-  
-    io.on('connection', async (socket) => {
-        socket.emit("connected")
-        socket.on('setUserId',({userId})=>{
-            if(!activeUsers.get(socket.id)){
-                console.log("Socket(New User)",userId)
-                activeUsers.set(socket.id,{userId})
-                console.log(activeUsers)
-            }
-        })
+  io.on("connection", async (socket) => {
+    socket.emit("connected");
+    socket.on("setUserId", ({ userId }) => {
+      if (!activeUsers.get(socket.id)) {
+        console.log("Socket(New User)", userId);
+        activeUsers.set(socket.id, { userId });
+        console.log(activeUsers);
+      }
+    });
 
-        socket.on('sendMessage',(data)=>{
-            sendMessage(data).then(([ids,sendData])=>{
-                broadCast(io,activeUsers,ids,'newMessage',sendData)
-            })
-        })
+    socket.on("sendMessage", (data) => {
+      sendMessage(data).then(([ids, sendData]) => {
+        broadCast(io, activeUsers, ids, "newMessage", sendData);
+      });
+    });
 
-        socket.on('disconnect', () => {
-            console.log('🔥: A user disconnected',socket.id);
-            activeUsers.delete(socket.id)
-        });
-        
-    })
-}
+    socket.on("disconnect", () => {
+      console.log("🔥: A user disconnected", socket.id);
+      activeUsers.delete(socket.id);
+    });
+  });
+};
